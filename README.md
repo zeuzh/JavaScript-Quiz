01c1ff88385f2cbf2e8b10df46caae44ccd469df
zeuz <zeuz@zeuzs-iMac.local>
2023-04-17 16:13:31 -0400

    Submiting quiz

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # JavaScript-Quiz
+GIVEN I am taking a code quiz
+ I click the start button
+ A timer starts and I am presented with a question
+ I answer a question
+ I am presented with another question
+ I answer a question incorrectly
+ Time is subtracted from the clock
+ All questions are answered or the timer reaches 0
+ The game is over
+ The game is over
+ I can save my initials and my score
--- script.js
+++ script.js
@@ -1,83 +1,150 @@
-const startBtn = document.querySelector(".startBtn")
-const startSection = document.querySelector(".start")
-const quiz = document.querySelector(".quiz")
-const question = document.querySelector(".question")
-const answerList = document.querySelector(".answerList")
+const startBtn = document.querySelector(".startBtn");
+const startSection = document.querySelector(".start");
+const quiz = document.querySelector(".quiz");
+const question = document.querySelector(".question");
+const answerList = document.querySelector(".answerList");
+var timeLeft = document.createElement("p");
 let index = 0;
 let score = 0;
 let time = 60;
-setTimeout(() => {
-console.log("hello")
-},60);
-let counter =0;
-setInterval(count,1000);
-function count () {
-    console.log(counter++);
-}
+
+function timer() {
+  quiz.append(timeLeft);
+  setInterval(function () {
+    time--;
+    timeLeft.innerHTML = "Time: " + time;
+    if (time <= 0) {
+      clearInterval(time);
+      endQuiz();
+      timeLeft.innerHTML = "Times Out!";
+    }
+  }, 1000)
+};
 
 const values = [
-    {
-        questionValue: "How do you create a varibale in javascript?",
-        answerValue: "C. var",
-        choicesValue: ["A. function", "B. querySelector", "C. var", "D. {}"]
-    },
-    {
-        questionValue: "What are the data types supported by JavaScript??",
-        answerValue: "All above",
-        choicesValue: ["A. Number", "B. Object", "C. String", "D. Symbol"]
-    },
-    {
-        questionValue: "Push Means?",
-        answerValue: "A. Add One or More Elements",
-        choicesValue: ["A. Add One or More Elements", "B. Specified Value", "C. Reverses", "D. String"]
-    
-    },
-    {
-]       questionValue: "Reverse Means?",
-        answerValue: "B. Elements of Array",
-        choicesValue: ["A. Index", "B. Elements of Array", "C. Function", "D. object"]
-   
-    },
-    {   
-        questionValue: "NAN means?",
-        answerValue: "C. Not a Number",
-        choicesValue: ["A. error", "B. Valid Number", "C. Not a Number", "D. Converted"]    
+  {
+    questionValue: "How do you create a varibale in javascript?",
+    answerValue: "C. var",
+    choicesValue: ["A. function", "B. querySelector", "C. var", "D. {}"],
+  },
+  {
+    questionValue: "What are the data types supported by JavaScript??",
+    answerValue: "All above",
+    choicesValue: ["A. Number", "B. Object", "C. String", "D. Symbol"],
+  },
+  {
+    questionValue: "Push Means?",
+    answerValue: "A. Add One or More Elements",
+    choicesValue: [
+      "A. Add One or More Elements",
+      "B. Specified Value",
+      "C. Reverses",
+      "D. String",
+    ],
+  },
+  {
+    questionValue: "Reverse Means?",
+    answerValue: "B. Elements of Array",
+    choicesValue: [
+      "A. Index",
+      "B. Elements of Array",
+      "C. Function",
+      "D. object",
+    ],
+  },
+  {
+    questionValue: "NAN means?",
+    answerValue: "C. Not a Number",
+    choicesValue: [
+      "A. error",
+      "B. Valid Number",
+      "C. Not a Number",
+      "D. Converted",
+    ],
+  },
+];
 
-
-startBtn.addEventListener("click", function() {
-    var timeLeft = document.createElement("p").innerHTML = "Time: " + time
-    quiz.append(timeLeft)
-    startSection.style.display = "none"
-    startQuiz(index)
-})
+startBtn.addEventListener("click", function () {
+  timer();
+  startSection.style.display = "none";
+  startQuiz(index);
+});
 
 function startQuiz(index) {
-    question.innerHTML = "";
-    answerList.innerHTML = "";
-    question.innerHTML = values[index].questionValue
-    const choices = values[index].choicesValue;
-    choices.forEach((i) => {
-        const li = document.createElement("li")
-        li.innerHTML = i;
-        answerList.append(li)
-        li.addEventListener("click", function(event) {
-            let clicked = event.target.innerHTML
-            if(clicked === values[index].answerValue) {
-                console.log("Correct")
-                score = score + 1
-            } else {
-                console.log("Wrong")
-                score = score - 1
-            }
-            console.log(score)
-            if(index < values.length - 1) {
-                index++;
-                startQuiz(index)
-            } else {
-                index = 0
-                console.log("Quiz is over")
-            }
-        })
-    })
+  question.innerHTML = "";
+  answerList.innerHTML = "";
+  question.innerHTML = values[index].questionValue;
+  const choices = values[index].choicesValue;
+  choices.forEach((i) => {
+    const li = document.createElement("li");
+    li.innerHTML = i;
+    answerList.append(li);
+    li.addEventListener("click", function (event) {
+      var outcome = document.createElement("p");
+      let clicked = event.target.innerHTML;
+      if (clicked === values[index].answerValue) {
+        console.log("Correct");
+        score = score + 1;
+        outcome.innerHTML = "Correct!";
+        quiz.append(outcome);
+      } else {
+        console.log("Wrong");
+        time = time - 10;
+        outcome.innerHTML = "Incorrect";
+        quiz.append(outcome);
+      }
+      console.log(score);
+      if (index < values.length - 1) {
+        setTimeout(function () {
+          index++;
+          outcome.innerHTML = "";
+          startQuiz(index);
+        }, 1000);
+      } else {
+        index = 0;
+        quiz.innerHTML = "";
+        endQuiz();
+      }
+    });
+  });
 }
 
+function endQuiz() {
+  var header = document.createElement("h1");
+  header.innerHTML = "Quiz Over! You got a score of: " + score;
+  quiz.append(header);
+  var input = document.createElement("input");
+  input.setAttribute("placeholder", "Initials");
+  quiz.append(input);
+  var button = document.createElement("button");
+  button.innerHTML = "Submit";
+  quiz.append(button);
+  button.addEventListener("click", function () {
+    var initials = input.value;
+    var obj = {
+      score: score,
+      initials: initials,
+    };
+    localStorage.setItem("score", JSON.stringify(obj));
+  })
+  saveInfo.addEventListener("click", function() {
+    var initials = userInitials.value;
+    if (initials === "") {
+        console.log("No value entered!");
+    } else {
+        var finalScore = {
+            initials: initials,
+            score: timeRemaining * 2
+        }
+        console.log(finalScore);
+        var allScores = localStorage.getItem("allScores");
+        if (allScores === null) {
+            allScores = [];
+        } else {
+            allScores = JSON.parse(allScores);
+        }
+        allScores.push(finalScore);
+        var newScore = JSON.stringify(allScores);
+        localStorage.setItem("allScores", newScore);
+        window.location.replace("scores.html");
+    }
